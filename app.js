0672b1092cd9d19accd87787db0513277a25bd15
Yarnold-rx7 <yrx_7@hotmail.com>
2024-03-21 16:30:10 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -25,3 +25,17 @@ items2.forEach((e) => {
     next = next.nextElementSibling;
   }
 });
+
+const items3 = document.querySelectorAll(".carousel3 .carousel-item3");
+items3.forEach((e) => {
+  const slide = 5;
+  let next = e.nextElementSibling;
+  for (let i = 0; i < slide; i++) {
+    if (!next) {
+      next = items3[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    e.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
