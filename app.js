86401e628bef068bfd123efcab514cd56b6d7d20
Yarnold-rx7 <yrx_7@hotmail.com>
2024-03-21 16:16:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-import { mnfs } from "./mnfs.js";
+const items = document.querySelectorAll(".carousel1 .carousel-item1");
+items.forEach((e) => {
+  const slide = 5;
+  let next = e.nextElementSibling;
+  for (let i = 0; i < slide; i++) {
+    if (!next) {
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    e.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
 
-console.log(mnfs);
+const items2 = document.querySelectorAll(".carousel2 .carousel-item2");
+items2.forEach((e) => {
+  const slide = 5;
+  let next = e.nextElementSibling;
+  for (let i = 0; i < slide; i++) {
+    if (!next) {
+      next = items2[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    e.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
